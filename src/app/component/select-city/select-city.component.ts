6d3381165b94968e13bc524eec1e699c43031a42
MaciejSzymanski300 <60852127+MaciejSzymanski300@users.noreply.github.com>
2020-11-18 13:28:29 +0100

    added functional left button(redirect to start)

--- src/app/component/select-city/select-city.component.ts
+++ src/app/component/select-city/select-city.component.ts
@@ -50,3 +50,20 @@ export class SelectCityComponent implements OnInit {
       'Something bad happened; please try again later.');
   }
 }
+// <!DOCTYPE html>
+// <html>
+//   <body>
+//
+//     <p>Click the button to display an alert box.</p>
+//
+// <button onclick="myFunction()">Try it</button>
+//
+// <script>
+// function myFunction() {
+//   alert("Hello! I am an alert box!");
+// }
+// </script>
+//
+// </body>
+// </html>
+
